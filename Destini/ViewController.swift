61fb044ae28217148dd5aebac057447e0270d6a4
Nathaniel Idahosa <eseos@icloud.com>
2018-07-19 12:51:07 +0200

    Added control flow

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -34,6 +34,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
+    var storyIndex : Int = 1
+    
     // TODO Step 5: Initialise instance variables here
     
     
@@ -41,7 +43,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
@@ -50,7 +54,73 @@ class ViewController: UIViewController {
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
+        
+        if storyIndex == 1 && sender.tag == 1 {
+            
+            storyIndex = 3
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            print(storyIndex) //3
+            
+        } else if storyIndex == 1 && sender.tag == 2 {
+            
+            storyIndex = 2
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+            
+        } else if storyIndex == 3 && sender.tag == 1 {
+            
+                storyIndex = 6
+                storyTextView.text = story6
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            
+        } else if storyIndex == 3 && sender.tag == 2 {
+            
+                storyIndex = 5
+                storyTextView.text = story5
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+        }
+        
+        
+        else {
+            
+            if storyIndex == 2 && sender.tag == 1 {
+                
+                storyIndex = 3
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+                
+            } else if storyIndex == 2 && sender.tag == 2 {
+                
+                storyIndex = 4
+                storyTextView.text = story4
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+                
+            } else if storyIndex == 3 && sender.tag == 1 {
+                
+                storyIndex = 6
+                storyTextView.text = story6
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+                
+            } else if storyIndex == 3 && sender.tag == 2 {
+                storyIndex = 5
+                storyTextView.text = story5
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+            }
+            
+        }
+            
+
+           
+        
         // TODO Step 4: Write an IF-Statement to update the views
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
